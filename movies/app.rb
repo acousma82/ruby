83f2cc76fa43e302c64c7da953f524e36dc74cc2
Admin <Sebastian_Peter@web.de>
2017-11-07 10:24:58 +0100

    dsfsadfasdfasdfdsfsadfasdfasdfdsfsadfasdfasdf

--- movies/app.rb
+++ movies/app.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
 require 'movie'
+require "movie_store"
+
+store = MovieStore.new("movies.yml")
 
 get('/movies') do
     @movies = []
@@ -17,9 +20,11 @@ erb :new
 end
 
 post('/movies/create') do
-   @movie = Movie.new
+  @movie = Movie.new
   @movie.title = params['title']
   @movie.director = params['director']
   @movie.year = params['year']
+  store.save(@movie)
+  redirect "/movies/new"
 end
 
